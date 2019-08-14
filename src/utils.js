0b6b931fadca9473409fb106a5b2e3bc626765bf
Alan Christopher Thomas <alan@tentmaker.co>
2019-08-14 12:48:49 -0500

    Make prismjs optional as highlighter

--- src/utils.js
+++ src/utils.js
@@ -2,19 +2,19 @@ import template from 'lodash/template';
 import markdownit from 'markdown-it';
 import Prism from 'prismjs';
 
-const md = new markdownit({
-  highlight: (str, lang) => {
-    if (lang) {
-      require(`prismjs/components/prism-${lang}`);
-      try {
-        return `<pre class="language-${lang}"><code>${Prism.highlight(str, Prism.languages[lang], lang)}</code></pre>`;
-      } catch (e) {}
-    }
-    return `<pre class="language-${lang}"><code>${md.utils.escapeHtml(str)}</code></pre>`;
-  }
-});
+export const toHTML = (content, data, options = {}) => {
+  const md = new markdownit({
+    highlight: options.highlight || ((str, lang) => {
+      if (lang) {
+        require(`prismjs/components/prism-${lang}`);
+        try {
+          return `<pre class="language-${lang}"><code>${Prism.highlight(str, Prism.languages[lang], lang)}</code></pre>`;
+        } catch (e) {}
+      }
+      return `<pre class="language-${lang}"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+    })
+  });
 
-export const toHTML = (content, data) => {
   const parse = template(content);
   const markdown = parse({
     identity_name: data.identity_name || 'example.internal.net',
