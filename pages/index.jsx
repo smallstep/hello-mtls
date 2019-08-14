f22de5e502931085159ea171a9a818358d6b5b58
Alan Christopher Thomas <alan@tentmaker.co>
2019-08-14 14:16:11 -0500

    Use link tag in next Link

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,15 +4,15 @@ import docs from '../src/docs';
 const Page = () => (
   <div>
     <h1>Docs</h1>
-  <ul>
-    {docs.map(doc => (
-      <li key="{doc}">
-        <Link href="/docs/[doc]" as={`/docs/${doc}`}>
-          {doc}
-        </Link>
-      </li>
-    ))}
-  </ul>
+    <ul>
+      {docs.map(doc => (
+        <li key="{doc}">
+          <Link href="/docs/[doc]" as={`/docs/${doc}`}>
+            <a>{doc}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
