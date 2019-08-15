ea462a7471b262b2829f9d847cec877ba88e3180
Alan Christopher Thomas <alan@tentmaker.co>
2019-08-15 16:32:03 -0500

    Merge verison and publish stages

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+git config --global user.email "alan@smallstep.com"
+git config --global user.name "Alan Thomas"
+git config --global push.default matching
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
+
+yarn version --patch --message "v%s [ci skip]"
+git push origin HEAD:$TRAVIS_BRANCH
+git push --tags
+
 echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > .npmrc
 
 yarn dist
