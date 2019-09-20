54e6567f2993efc5119b4cb856e8bb681830c692
Alan Christopher Thomas <alan@tentmaker.co>
2019-09-20 13:52:55 -0500

    Ignore dist directory in tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+  ],
 };
