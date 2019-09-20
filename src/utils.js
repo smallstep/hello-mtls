b208e93a6eaa72243e035b7684330bb46b347262
Alan Christopher Thomas <alan@tentmaker.co>
2019-09-20 13:53:31 -0500

    Use client and server-specific filenames as defaults

--- src/utils.js
+++ src/utils.js
@@ -8,12 +8,12 @@ export const parseTemplate = (content, data = {}) => {
   // interpolate variables into markdown template
   return Mustache.render(content, {
     server_name: data.server_name || 'example.internal.net',
-    server_cert: data.server_cert || 'example.crt',
-    server_key: data.server_key || 'example.key',
+    server_cert: data.server_cert || 'server.crt',
+    server_key: data.server_key || 'server.key',
     server_port: data.server_port || 443,
     client_name: data.client_name || 'example.internal.net',
-    client_cert: data.client_cert || 'example.crt',
-    client_key: data.client_key || 'example.key',
+    client_cert: data.client_cert || 'client.crt',
+    client_key: data.client_key || 'client.key',
     ca_cert: data.ca_cert || 'ca.crt',
   });
 };
