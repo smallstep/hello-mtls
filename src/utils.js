01a7d2b0d400215a06bfd2c86a4bad03c8e80e65
Alan Christopher Thomas <alan@tentmaker.co>
2019-09-27 10:41:45 -0500

    Make server and client defaults distinguishable

--- src/utils.js
+++ src/utils.js
@@ -7,11 +7,11 @@ export const listDocs = () => docs;
 export const parseTemplate = (content, data = {}) => {
   // interpolate variables into markdown template
   return Mustache.render(content, {
-    server_name: data.server_name || 'example.internal.net',
+    server_name: data.server_name || 'server.internal.net',
     server_cert: data.server_cert || 'server.crt',
     server_key: data.server_key || 'server.key',
     server_port: data.server_port || 443,
-    client_name: data.client_name || 'example.internal.net',
+    client_name: data.client_name || 'clientuser',
     client_cert: data.client_cert || 'client.crt',
     client_key: data.client_key || 'client.key',
     ca_cert: data.ca_cert || 'ca.crt',
